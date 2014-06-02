629c632bffcb7fafffa311749dbafed232f51b68
Ancor Gonzalez Sosa <ancor@suse.de>
2014-06-02 18:50:15 +0200

    Changes in OpenqaJob#find_all_by for more RESTlike caching
     * Goes together with https://github.com/os-autoinst/openQA/pull/90
       (if not, can lead to some unexpected behavior)
     * In addition to the already existing /openqa_jobs/q[iso]=ISO
       Now /openqa_jobs (without filters) also caches (and refreshes if needed)
       the job lists per iso name.
     * It looks more RESTlike since it works coherently with and without filter.
     * It allows to refresh the whole cache in a single call

--- app/models/obs_factory/openqa_job.rb
+++ app/models/obs_factory/openqa_job.rb
@@ -4,7 +4,7 @@ module ObsFactory
     extend ActiveModel::Naming
     include ActiveModel::Serializers::JSON
 
-    attr_accessor :id, :name, :state, :result, :clone_id
+    attr_accessor :id, :name, :state, :result, :clone_id, :iso
 
     def self.openqa_base_url
       "https://openqa.opensuse.org"
@@ -12,28 +12,56 @@ module ObsFactory
 
     @@api = ObsFactory::OpenqaApi.new(openqa_base_url)
 
+    # Works in a similar way to ActiveRecord::Base#find_all_by
+    # If searching by iso or getting the full list, caching comes into play. In
+    # any other case, a GET query to openQA is always performed.
+    # :cache == 'refresh' can be used in the 'opt' (second param) to force a
+    # refresh of the cache.
     def self.find_all_by(args = {}, opt = {})
+      refresh = (opt.symbolize_keys[:cache].to_s == 'refresh')
+      filter = args.symbolize_keys.slice(:iso, :state, :build, :maxage)
 
       # We are only interested in current results
       get_params = {scope: 'current'}
 
-      if iso = args[:iso_name]
-        get_params[:iso] = iso
-        cache_entry = "openqa_jobs_for_iso_#{iso}"
+      # If searching for the whole list of jobs, it caches the jobs
+      # per ISO name
+      if filter.empty?
+        Rails.cache.delete('openqa_isos') if refresh
+        jobs = []
+        cached = true
+        isos = Rails.cache.fetch('openqa_isos', expires_in: 20.minutes) do
+          cached = false
+          jobs = @@api.get('jobs', get_params)['jobs']
+          jobs.group_by {|j| j['iso']}.each do |iso, iso_jobs|
+            Rails.cache.write("openqa_jobs_for_iso_#{iso}", iso_jobs)
+          end
+          jobs.map {|j| j['iso']}.sort.uniq
+        end
+        if cached
+          (isos + [nil]).each do |iso|
+            jobs += Rails.cache.read("openqa_jobs_for_iso_#{iso}") || []
+          end
+        end
+      # If searching only by ISO, cache that one
+      elsif filter.keys == [:iso]
+        get_params[:iso] = filter[:iso]
+        cache_entry = "openqa_jobs_for_iso_#{filter[:iso]}"
+        Rails.cache.delete(cache_entry) if refresh
+        jobs = Rails.cache.fetch(cache_entry, expires_in: 20.minutes) do
+          @@api.get('jobs', get_params)['jobs']
+        end
+      # In any other case, don't cache
       else
-        # FIXME: it's almost for sure too big for a single memcached entry
-        cache_entry = "openqa_jobs"
+        get_params.merge!(filter)
+        jobs = @@api.get('jobs', get_params)['jobs']
       end
 
-      Rails.cache.delete(cache_entry) if opt.symbolize_keys[:cache].to_s == 'refresh'
-      jobs = Rails.cache.fetch(cache_entry, expires_in: 20.minutes) do
-        @@api.get('jobs', get_params)['jobs']
-      end
       jobs.map { |j| OpenqaJob.new(j.slice(*attributes)) }
     end
 
     def self.attributes
-      %w(id name state result clone_id)
+      %w(id name state result clone_id iso)
     end
 
     # Required by ActiveModel::Serializers
