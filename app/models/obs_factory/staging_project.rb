1c4cd63d7dec7f10f932b7194a8e98eeb42ba629
Ancor Gonzalez Sosa <ancor@suse.de>
2014-06-03 15:25:50 +0200

    Add build status information to StagingProject model and dashboard

--- app/models/obs_factory/staging_project.rb
+++ app/models/obs_factory/staging_project.rb
@@ -35,6 +35,16 @@ module ObsFactory
       @openqa_jobs ||= iso.nil? ? [] : OpenqaJob.find_all_by(iso: iso)
     end
 
+    def broken_packages
+      set_buildinfo if @broken_packages.nil?
+      @broken_packages
+    end
+
+    def building_repositories
+      set_buildinfo if @building_repositories.nil?
+      @building_repositories
+    end
+
     def iso
       return @iso if @iso
       buildresult = Buildresult.find_hashed(project: name, package: 'Test-DVD-x86_64',
@@ -49,14 +59,42 @@ module ObsFactory
       @iso = "openSUSE-Staging:#{letter}-#{suffix}-DVD-x86_64-#{ending}"
     end
 
-    # Coming next: untracked_requests, unreviewed_requests, buildstatus
+    # Coming next: untracked_requests, unreviewed_requests
     def self.attributes
-      %w(name description obsolete_requests openqa_jobs subprojects)
+      %w(name description obsolete_requests openqa_jobs building_repositories broken_packages subprojects)
     end
 
     # Required by ActiveModel::Serializers
     def attributes
       Hash[self.class.attributes.map { |a| [a, nil] }]
+    end
+
+    protected
+
+    def set_buildinfo
+      buildresult = Buildresult.find_hashed(project: name, code: %w(failed broken unresolvable))
+      @broken_packages = []
+      @building_repositories = []
+      buildresult['result'].each do |result|
+        building = false
+        if !%w(published unpublished).include?(result['state']) || result['dirty'] == 'true'
+          building = true
+          @building_repositories << result.slice('repository', 'arch', 'code', 'state', 'dirty')
+        end
+        if statuses = result['status']
+          statuses.each do |status|
+            if status.kind_of?(Hash) && code = status['code']
+              if %w(broken failed).include?(code) || (code == 'unresolvable' && !building)
+                @broken_packages << { 'package' => status['package'],
+                                      'state' => code,
+                                      'details' => status['details'],
+                                      'repository' => result['repository'],
+                                      'arch' => result['arch'] }
+              end
+            end
+          end
+        end
+      end
     end
   end
 end
