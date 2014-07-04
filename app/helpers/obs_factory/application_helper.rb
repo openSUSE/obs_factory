389c87d41a6464dbffe5065db761d19c10c41269
Ancor Gonzalez Sosa <ancor@suse.de>
2014-07-04 13:15:52 +0200

    Adjustments to the previous commit
    To sync with the changes introduced in the second commit on
    https://github.com/openSUSE/open-build-service/pull/725

--- app/helpers/obs_factory/application_helper.rb
+++ app/helpers/obs_factory/application_helper.rb
@@ -3,8 +3,8 @@ module ObsFactory
 
     # Catch some url helpers used in the OBS layout and forward them to
     # the main application
-    %w(root_path project_show_path search_path user_show_url user_show_path logout_path
-       news_feed_path public_projects_path monitor_path).each do |m|
+    %w(root_path project_show_path search_path user_show_url user_show_path user_logout_path
+       news_feed_path project_list_public_path monitor_path).each do |m|
       define_method(m) do |*args|
         main_app.send(m, args)
       end
