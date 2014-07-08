cbc7837693160942d353d034d0d76a3d662dc414
Ancor Gonzalez Sosa <ancor@suse.de>
2014-07-08 17:50:51 +0200

    Don't use asset pipeline (doesn't work with our OBS packages)
     * The user running the application should have permissions to write in
       the 'public/stylesheets' directory of the host application.
     * application.css (good old CSS now) is copied there during application
       startup.
     * A version number is used to emulate some of the advantages of asset pipeline
       digesting. Should be manually updated when changing application.css. We can
       change to a better solution afterward.

--- lib/obs_factory/engine.rb
+++ lib/obs_factory/engine.rb
@@ -1,5 +1,17 @@
 module ObsFactory
+  # Poor man's asset digest
+  STATIC_VERSION = 1
+
   class Engine < ::Rails::Engine
     isolate_namespace ObsFactory
+
+    initializer :copy_static do |app|
+      unless app.root.to_s.match root.to_s
+        target = File.join(app.root.to_s, 'public', 'stylesheets', 'obs_factory')
+        source = File.join(root.to_s, 'public', 'stylesheets', 'application.css')
+        FileUtils.mkdir_p(target)
+        FileUtils.cp(source, File.join(target, "application-#{ObsFactory::STATIC_VERSION}.css"))
+      end
+    end
   end
 end
