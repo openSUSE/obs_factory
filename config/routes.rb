5d52df1a24791e528e7d1e0939b4cad43ba361f8
Ancor Gonzalez Sosa <ancor@suse.de>
2014-08-25 09:50:08 +0000

    Adapt routes to be mounted on /
     * New routes follow the OBS convention /project/:action/:project_name
     * Redirects for the old /factory routes
     * Everything in place to draw the expected breadcrumbs
     * A minor fix in the dashboard view

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 ObsFactory::Engine.routes.draw do
-  resources :distributions, only: [:show], constraints: {id: %r{[^\/]*}} do
-    # Status of staging projects (as status pages or as JSON structures)
-    resources :staging_projects, only: [:index, :show], controller: :staging_projects
-  end
+  cons = { project: %r{[^\/]*} }
+  get 'project/dashboard/:project' => "distributions#show", as: 'dashboard', constraints: cons
+  get 'project/staging_projects/:project' => "staging_projects#index", as: 'staging_projects', contraints: cons
+  get 'project/staging_projects/:project/:id' => "staging_projects#show", as: 'staging_project', contraints: cons
+
   # Used to enforce the refresh of the cache of jobs (using cache=refresh)
-  get '/openqa_jobs' => 'openqa_jobs#index'
+  get 'openqa_jobs' => 'openqa_jobs#index'
+
+  # Compatibility with old build.opensuse.org routes
+  old_mount_point = 'factory'
+  get "#{old_mount_point}/dashboard", to: redirect('project/dashboard/openSUSE:Factory')
+  get "#{old_mount_point}/staging_projects", to: redirect('project/staging_projects/openSUSE:Factory')
+  get "#{old_mount_point}/staging_projects/:id", to: redirect('project/staging_projects/openSUSE:Factory/%{id}')
+  get "#{old_mount_point}/openqa_jobs", to: redirect('openqa_jobs')
 end
