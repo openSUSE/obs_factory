cb6237e13d3e477e599e3cdc1b7e73f0ac1cbb80
Ruediger Oertel <ro@suse.de>
2017-12-04 15:56:32 +0100

    also send method user_rss_notifications_url to main app

--- app/helpers/obs_factory/application_helper.rb
+++ app/helpers/obs_factory/application_helper.rb
@@ -5,7 +5,8 @@ module ObsFactory
     # the main application
     %w(root_path project_show_path search_path user_show_url user_show_path user_logout_path
        user_login_path user_register_user_path user_do_login_path news_feed_path project_toggle_watch_path
-       project_list_public_path monitor_path projects_path new_project_path).each do |m|
+       project_list_public_path monitor_path projects_path new_project_path
+       user_rss_notifications_url).each do |m|
       define_method(m) do |*args|
         main_app.send(m, *args)
       end
