8346aade170ce18fc098641ea1f7a95ea3b6a6de
Björn Geuken <bgeuken@suse.de>
2018-04-05 08:02:36 +0200

    Replace user_* routes with session_* routes
    The OBS routes has been changed with
      https://github.com/openSUSE/open-build-service/pull/4731

--- app/helpers/obs_factory/application_helper.rb
+++ app/helpers/obs_factory/application_helper.rb
@@ -3,10 +3,10 @@ module ObsFactory
 
     # Catch some url helpers used in the OBS layout and forward them to
     # the main application
-    %w(home_path user_tasks_path root_path project_show_path search_path user_show_url user_show_path user_logout_path
-       user_login_path user_register_user_path user_do_login_path news_feed_path project_toggle_watch_path
+    %w(home_path user_tasks_path root_path project_show_path search_path user_show_url user_show_path
+       user_register_user_path news_feed_path project_toggle_watch_path
        project_list_public_path monitor_path projects_path new_project_path
-       user_rss_notifications_url).each do |m|
+       user_rss_notifications_url session_new_path session_create_path session_destroy_path).each do |m|
       define_method(m) do |*args|
         main_app.send(m, *args)
       end
