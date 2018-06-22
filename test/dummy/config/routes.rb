219a6328f3b82a8a4ed2cda6a996cb0485b8c6a8
Stephan Kulow <coolo@suse.de>
2018-06-22 19:44:28 +0200

    Replace repo with README pointing to build service
    Fixes #142

--- test/dummy/config/routes.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.routes.draw do
-
-  mount ObsFactory::Engine => "/obs_factory"
-end
--- test/obs_factory_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ObsFactoryTest < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, ObsFactory
-  end
-end
